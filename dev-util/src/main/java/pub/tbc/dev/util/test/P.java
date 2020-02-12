0404abd407d2dd1dcfbc6597fc42e338c588675d
tbc <tbc521@163.com>
2020-02-12 12:03:15 +0800

    print util, add printf, println, update print

--- dev-util/src/main/java/pub/tbc/dev/util/test/P.java
+++ dev-util/src/main/java/pub/tbc/dev/util/test/P.java
@@ -1,5 +1,7 @@
 package pub.tbc.dev.util.test;
 
+import java.util.function.Consumer;
+
 /**
  * 控制台日志输出辅助类（测试调试用，禁止生产环境使用）
  *
@@ -7,26 +9,45 @@ package pub.tbc.dev.util.test;
  */
 public final class P {
     // @formatter:off
-    /** 占位符 */
+    /** 占位符: {} */
     public static final String PLACEHOLDER = "\\{}";
     /** 默认调用栈数组开始打印的下标 */
     public static final int DEFAULT_STACK_START_INDEX = 2;
     // @formatter:on
 
-    public static StackTraceElement[] stackTraceElements() {
-        return Thread.currentThread().getStackTrace();
-    }
-
-    /**
-     * 格式化日志输出
-     */
-    public static void print(String msg, Object... params) {
+    private static String msg(String msg, Object... params) {
         if (params != null && params.length > 0) {
             for (Object param : params) {
                 msg = msg.replaceFirst(PLACEHOLDER, param == null ? "null" : param.toString());
             }
         }
-        System.out.println(msg);
+        return msg;
+    }
+
+    public static StackTraceElement[] stackTraceElements() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    public static void printf(String msg, Object... params) {
+        System.out.printf(msg, params);
+    }
+
+    public static void print0(String msg, Consumer<String> consumer) {
+        consumer.accept(msg);
+    }
+
+    /**
+     * 格式化日志输出，换行
+     */
+    public static void println(String msg, Object... params) {
+        print0(msg(msg, params), System.out::println);
+    }
+
+    /**
+     * 格式化日志输出，不换行
+     */
+    public static void print(String msg, Object... params) {
+        print0(msg(msg,params), System.out::print);
     }
 
     public static void print(String msg) {
